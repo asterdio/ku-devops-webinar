c8cb321296e38037d43e0cc2d35bb2a14d7926d3
rkapali <81683829+rkapali@users.noreply.github.com>
2023-09-14 00:05:11 +0545

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello KU Hackathon 2023!';
+    return 'Hello to everyone at KU Hackathon 2023!';
   }
 }
