55397eeeab9b077bbe8fa6c9d7abb63f2c17946d
rkapali <81683829+rkapali@users.noreply.github.com>
2023-09-14 01:27:11 +0545

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello to everyone at KU Hackathon 2023!';
+    return 'Hi Aman, CI is implemented but I haven't used docker-compose.yml. Check the ci.yml for changes. Hello to everyone at KU Hackathon 2023!';
   }
 }
