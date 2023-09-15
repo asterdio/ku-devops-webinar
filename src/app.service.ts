3e471745dde2b785f106e60a356d29f404452f43
amanasterdio <aman@asterdio.com>
2023-09-15 14:31:38 +0545

    ci: docker build process testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello to everyone at KU Hackathon 2023!';
+    return 'Hello to everyone at KU Hackathon 2023! We welcome you to this webinar regarding the 12 factor Apps Methodology and its use in Cloud-Native Environment';
   }
 }
